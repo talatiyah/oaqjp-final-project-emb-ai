e2419962199dab49d133e7a1779c1ac181b8c05f
talatiyah <wataniq8.com@gmail.com>
2026-09-08 11:25:46 +0300

    Create emotion_detection.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    headers = {"grpc-metadata-mm-model-id":"emotion_aggregated-workflow_lang_en_stock"}
+
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=input_json
+    )
+
+    data = json.loads(response.text)
+    emotions = data["emotionPredictions"][0]["emotion"]
+
+    emotions["dominant_emotion"] = max(emotions, key=emotions.get)
+
+    return emotions
