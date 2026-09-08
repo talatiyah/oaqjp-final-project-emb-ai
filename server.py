5f9a22efba100e855aac1a53dd732069c32734fa
talatiyah <wataniq8.com@gmail.com>
2026-09-08 22:31:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,6 +14,9 @@ def detect_emotion():
     text = request.args.get("textToAnalyze")
     result = emotion_detector(text)
 
+    if result["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+
     return f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} and 'sadness': {result['sadness']}. The dominant emotion is <b>{result['dominant_emotion']}.</b>"
 
 
