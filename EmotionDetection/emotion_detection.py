522f4a2714093580e892f37947cfefcc0639f098
talatiyah <wataniq8.com@gmail.com>
2026-09-08 22:31:46 +0300

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,6 +14,15 @@ def emotion_detector(text_to_analyze):
         headers=headers,
         json=input_json
     )
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+           "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
 
     data = json.loads(response.text)
     emotions = data["emotionPredictions"][0]["emotion"]
